e73573e174e8db4a34732af49989c90b44d24d63
Matteojk95 <matteopalmer2095@gmail.com>
2023-09-04 13:42:45 +0200

    non mi prende le eccezioni

--- src/main/java/org/biglietteria/Biglietteria.java
+++ src/main/java/org/biglietteria/Biglietteria.java
@@ -1,7 +1,31 @@
 package org.biglietteria;
 
+import java.util.Scanner;
+
 public class Biglietteria {
     public static void main(String[] args) {
+
+        Scanner scan= new Scanner(System.in);
+        boolean isValid=false;
+        while (!isValid) {
+            System.out.println( "inserisci quanti chilometri devi fare");
+            int chilometri = Integer.parseInt(scan.nextLine());
+            System.out.println( "inserisci la tua eta");
+            int eta = Integer.parseInt(scan.nextLine());
+            try {
+                Biglietto biglietto = new Biglietto(chilometri, eta);
+                isValid = true;
+                System.out.println("biglietto valido");
+                System.out.println(biglietto.calcolaPrezzo());
+            } catch (RuntimeException e ){
+                System.out.println("il numero di chilometri non può essere negativo");
+            } catch (Exception e ){
+                System.out.println("eta non può essere negativa");
+            }
+        }
+
+
+
 
     }
 }
